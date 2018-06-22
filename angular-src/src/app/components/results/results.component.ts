a59c3cf98728142a4eb72be99b603b76c9aec97b
Vincent Chho <vincec362@gmail.com>
2018-06-22 14:56:28 -0700

    results.component.ts changes

--- angular-src/src/app/components/results/results.component.ts
+++ angular-src/src/app/components/results/results.component.ts
@@ -12,12 +12,23 @@ export class ResultComponent implements OnInit {
 
     public storedResults:any;
     public resultSub: Subscription;
+    public columnsToDisplay: String[] = [];
 
     constructor(private _search: SearchService) {}
 
     ngOnInit(): void {
         // this._search.getResults().subscribe(results => this.storedResults = results)
-        this._search.resultsSubscription().subscribe(results => this.storedResults = results.data);
+        this._search.resultsSubscription().subscribe(results => {
+            console.log('receiving data', results);
+            // this.storedResults = results;
+            this.storedResults = results.data.result;
+            console.log('stored results', this.storedResults);
+
+            this.columnsToDisplay = [];
+            for(let key in this.storedResults[0]){
+                console.log(this.storedResults);
+            }
+        });
     }
     
 }
